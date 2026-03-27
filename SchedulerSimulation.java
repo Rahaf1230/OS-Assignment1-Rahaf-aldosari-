b939cfe0aea7575fc6c2ebc20d293da6e6596cea
Rahaf1230 <445052029@std.psau.edu.sa>
2026-03-27 03:53:15 +0300

    feature3: added waiting time calculation and reporting

--- SchedulerSimulation.java
+++ SchedulerSimulation.java
@@ -30,6 +30,9 @@ class Process implements Runnable {
     private int timeQuantum; // Time slice (time quantum) allowed per CPU access (in milliseconds)
     private int remainingTime; // Time left for the process to finish its execution
     int priority;
+    // Feature 3: Implemented waiting time calculation and summary report
+    private long creationTime;
+    private long waitingTime;
 
     // Constructor to initialize the process with name, burst time, and time quantum
     public Process(String name, int burstTime, int timeQuantum) {
@@ -37,7 +40,10 @@ class Process implements Runnable {
         this.burstTime = burstTime;
         this.timeQuantum = timeQuantum;
         this.remainingTime = burstTime; // Initially, remaining time is equal to the burst time
-        this.priority = (int) (Math.random() * 5) + 1;
+        this.priority = (int) (Math.random() * 5) + 1;// feature 1
+        // Feature 3: Implemented waiting time calculation and summary report
+        this.creationTime = System.currentTimeMillis();
+        this.waitingTime = 0;
 
     }
 
@@ -75,6 +81,9 @@ class Process implements Runnable {
         }
 
         remainingTime -= runTime; // Deduct the run time from the remaining time
+
+        waitingTime = System.currentTimeMillis() - creationTime; // Feature 3: Implemented waiting time calculation and
+                                                                 // summary report
         int overallProgress = (int) (((double) (burstTime - remainingTime) / burstTime) * 100);
         String overallProgressBar = createProgressBar(overallProgress, 20);
 
@@ -144,6 +153,12 @@ class Process implements Runnable {
     // Check if the process has finished (i.e., no remaining time)
     public boolean isFinished() {
         return remainingTime <= 0;
+
+    }
+
+    // Feature 3: Implemented waiting time calculation and summary report
+    public long getwaitingTime() {
+        return waitingTime;
     }
 }
 
@@ -290,6 +305,15 @@ public class SchedulerSimulation {
                 "╚════════════════════════════════════════════════════════════════════════════════╝" +
                 Colors.RESET + "\n");
         System.out.println("Total context switches: " + contextSwitches); // feature2 display total
+
+        // Feature 3: Implemented waiting time calculation and summary report
+        System.out.println("\nProcess Summary:");
+        for (Process P : processMap.values()) {
+            System.out.println(P.getName() +
+                    " |Burst: " + P.getBurstTime() +
+                    " | Waiting: " + P.getwaitingTime() + "ms");
+
+        }
     }
 
     // Method to add a process to the queue and map, while printing a "ready"
